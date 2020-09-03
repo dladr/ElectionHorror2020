9d13ab636e1d2f0f59d6a5b01e48ef1c616a70a7
Dave <dladr@hotmail.com>
2020-09-03 15:41:12 -0700

    Lights, mirrors, physics... OH MY!

---
+++ Assets/RotateWithXInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithXInput : MonoBehaviour
+{
+    [SerializeField] private float maxYRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateTransform(Input.GetAxis("Horizontal"));
+    }
+
+    void RotateTransform(float xInput)
+    {
+        transform.localEulerAngles = new Vector3(0, xInput * maxYRotation, 0);
+    }
+}
