b1328fe87a8d1280270cbca09089a80b26a170f2
Dave <dladr@hotmail.com>
2020-09-05 16:00:56 -0700

    improving player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed;
 
     [SerializeField] private bool _isActive;
+    [SerializeField] private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
     void Start()
@@ -21,17 +22,24 @@ public class PlayerController : MonoBehaviour
         if (!_isActive)
             return;
 
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
         Move(new Vector2(xInput, yInput).normalized);
+     // Move(new Vector3(xInput, 0, yInput));
     }
 
     void Move(Vector2 inputDirection)
     {
-        if (inputDirection == Vector2.zero)
-            return;
+     //   transform.position += transform.forward * inputDirection.y * _speed * Time.deltaTime + transform.right * inputDirection.x * _speed * Time.deltaTime;
+        //_rigidbody.AddForce(transform.forward * inputDirection.y * _speed, ForceMode.VelocityChange);
+        //_rigidbody.AddForce(transform.right * inputDirection.x * _speed, ForceMode.VelocityChange);
+        _rigidbody.velocity = transform.forward * inputDirection.y * _speed +
+                              transform.right * inputDirection.x * _speed;
+    }
 
-        transform.position += transform.forward * inputDirection.y * _speed * Time.deltaTime + transform.right * inputDirection.x * _speed * Time.deltaTime;
+    void Move(Vector3 inputDirection)
+    {
+        transform.localPosition += inputDirection * _speed * Time.deltaTime;
     }
 
     [Button]
