1249f022cf8624dee3bf4d499c1a811bfaeb1e9f
Dave <dladr@hotmail.com>
2020-09-25 23:28:41 -0700

    Minor sequence work

--- Assets/GrandmaHouseSequence.cs
+++ Assets/GrandmaHouseSequence.cs
@@ -73,6 +73,123 @@ public class GrandmaHouseSequence : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[4], _dialogueColors[4], _dialogueFontStyles[4]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[5], _dialogueColors[5], _dialogueFontStyles[5]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[6], _dialogueColors[6], _dialogueFontStyles[6]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[7], _dialogueColors[7], _dialogueFontStyles[7]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[8], _dialogueColors[8], _dialogueFontStyles[8]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[9], _dialogueColors[9], _dialogueFontStyles[9]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+
+        Debug.Log("Granny started coughing");
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        Debug.Log("Granny stopped coughing");
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[10], _dialogueColors[10], _dialogueFontStyles[10]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[11], _dialogueColors[11], _dialogueFontStyles[11]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[12], _dialogueColors[12], _dialogueFontStyles[12]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(_dialogueStrings[13], _dialogueColors[13], _dialogueFontStyles[13]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        Debug.Log("Time To Vote");
+
         yield return null;
     }
 }
