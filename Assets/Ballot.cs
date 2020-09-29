bc4c52ca0a6d0022412f9b677cd53c55ce53c428
Dave <dladr@hotmail.com>
2020-09-29 09:34:36 -0700

    Ballot

---
+++ Assets/Ballot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.Utilities;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Ballot : MonoBehaviour
+{
+    [SerializeField] private EventSystem _eventSystem;
+    [SerializeField] private GameObject _lastSelectedGameObject;
+    [SerializeField] List<bool> _ballotAnswers;
+    [SerializeField] private GameManager _gameManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_eventSystem.currentSelectedGameObject.SafeIsUnityNull())
+            _eventSystem.SetSelectedGameObject(_lastSelectedGameObject);
+
+        _lastSelectedGameObject = _eventSystem.currentSelectedGameObject;
+    }
+
+    public void UpdateBallotAnswer(string answer)
+    {
+        int questionNumber = int.Parse(answer[0].ToString());
+        bool answerBool = answer[1] == 't' ? true : false;
+        _ballotAnswers[questionNumber] = answerBool;
+    }
+
+    public void SubmitBallotToGameManager()
+    {
+        _gameManager.BallotAnswers = _ballotAnswers;
+    }
+}
