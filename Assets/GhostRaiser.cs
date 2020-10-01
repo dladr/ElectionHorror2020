e7c74fdce2f0d30165bb73782b8bb1fd124617ce
Dave <dladr@hotmail.com>
2020-09-30 18:26:56 -0700

    Working to raise the ghost

---
+++ Assets/GhostRaiser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostRaiser : MonoBehaviour
+{
+    [SerializeField] Transform _ghostFrontTransform;
+    [SerializeField] private SpriteColorManipulator _spriteColorManipulator;
+
+    [SerializeField] private Transform _bodyFrontTransform;
+    [SerializeField] private Transform _bodyTransform;
+
+    [SerializeField] Transform[] _ghostTransformsToManipulate;
+
+    private void Awake()
+    {
+        _ghostTransformsToManipulate = _ghostFrontTransform.GetComponentsInChildren<Transform>();
+    }
+
+    private void Start()
+    {
+        _spriteColorManipulator.UpdateSpriteRendererAlphas(0);
+    }
+
+    public void RaiseGhost()
+    {
+        MatchTransforms();
+        _spriteColorManipulator.CallChangeAlphaOverTime(10, 100);
+    }
+
+    void MatchTransforms()
+    {
+        transform.position = _bodyTransform.position;
+        transform.eulerAngles = _bodyTransform.eulerAngles;
+
+        Transform[] bodyfrontTransforms = _bodyFrontTransform.GetComponentsInChildren<Transform>();
+
+        foreach (Transform transform1 in _ghostTransformsToManipulate)
+        {
+            foreach (Transform bodyfrontTransform in bodyfrontTransforms)
+            {
+                if (transform1.name == bodyfrontTransform.name)
+                {
+                    transform1.position = bodyfrontTransform.position;
+                    transform1.eulerAngles = bodyfrontTransform.eulerAngles;
+                }
+            }
+        }
+
+        transform.position += new Vector3(.001f, 0, -.003f);
+    }
+
+
+}
--- Assets/SpriteColorManipulator.cs
+++ Assets/SpriteColorManipulator.cs
@@ -27,6 +27,39 @@ public class SpriteColorManipulator : MonoBehaviour
         }
     }
 
+  public void UpdateSpriteRendererAlphas(float alpha)
+  {
+      foreach (SpriteRenderer spriteRenderer in _spriteRenderers)
+      {
+          var color = spriteRenderer.color;
+          color = new Color(color.r, color.g, color.b, alpha);
+          spriteRenderer.color = color;
+      }
+  }
+
+  public void CallChangeAlphaOverTime(float timeToChange, float alpha)
+  {
+      StartCoroutine(ChangeAlphaOverTime(timeToChange, alpha));
+  }
+
+  IEnumerator ChangeAlphaOverTime(float timeToChange, float alpha)
+  {
+      float timeElapsed = 0;
+      float startingAlpha = _spriteRenderers[0].color.a;
+
+      while (timeElapsed < timeToChange)
+      {
+          timeElapsed += Time.deltaTime;
+          float lerpPercent = timeElapsed / timeToChange;
+
+          UpdateSpriteRendererAlphas(Mathf.Lerp(startingAlpha, alpha, lerpPercent));
+
+          yield return new WaitForEndOfFrame();
+      }
+
+      yield return null;
+  }
+
     // Update is called once per frame
     void Update()
     {
