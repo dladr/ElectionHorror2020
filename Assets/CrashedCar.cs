39de111ac6463ca96a2171857bf8b3ca2791bf18
Dave <dladr@hotmail.com>
2020-10-10 18:16:41 -0700

    Stranger Sitting in Truck
    Not injured enough...

--- Assets/CrashedCar.cs
+++ Assets/CrashedCar.cs
@@ -10,9 +10,9 @@ public class CrashedCar : MonoBehaviour
 
     public string[] DialogueStrings;
 
-    public Color TextColor;
+    public Color[] TextColors;
 
-    public FontStyles FontStyles;
+    public FontStyles[] FontStyles;
 
     private TextModifier _textModifier;
 
@@ -22,20 +22,38 @@ public class CrashedCar : MonoBehaviour
 
     private ScreenFader _screenFader;
 
+    public Transform _playerLocation;
+    public Transform _truckLocation;
+    public GameObject TruckGameObject;
+
+    private RearDoor _rearDoor;
+
+    private bool _hasPlayerHitSpace;
+
+    private PlayerController _playerController;
+
+
+
     // Start is called before the first frame update
     void Awake()
     {
         _textModifier = SingletonManager.Get<TextModifier>();
         _orbManager = SingletonManager.Get<OrbManager>();
         _screenFader = SingletonManager.Get<ScreenFader>();
+        _rearDoor = SingletonManager.Get<RearDoor>();
+        _playerController = SingletonManager.Get<PlayerController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Action"))
+            _hasPlayerHitSpace = true;
+
         if (_isPlayerPresent && Input.GetButtonDown("Action"))
         {
-            
+            StartCoroutine(CrashSequence());
         }
     }
 
@@ -44,7 +62,7 @@ public class CrashedCar : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             _isPlayerPresent = true;
-            _textModifier.UpdateTextTrio(Title, TextColor, FontStyles);
+            _textModifier.UpdateTextTrio(Title, TextColors[0], FontStyles[0]);
             _textModifier.Fade(true, 10);
             _orbManager.SetCanAttack(false);
         }
@@ -58,5 +76,56 @@ public class CrashedCar : MonoBehaviour
             _textModifier.Fade(false, 10);
             _orbManager.SetCanAttack(true);
         }
+    }
+
+    IEnumerator CrashSequence()
+    {
+        _playerController.Deactivate();
+
+        _textModifier.UpdateTextTrio(DialogueStrings[0], TextColors[0], FontStyles[0]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(DialogueStrings[1], TextColors[1], FontStyles[1]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _textModifier.UpdateTextTrio(DialogueStrings[2], TextColors[2], FontStyles[2]);
+        _textModifier.Fade();
+        _hasPlayerHitSpace = false;
+        while (!_hasPlayerHitSpace)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        _textModifier.Fade(false);
+        yield return new WaitForSeconds(.5f);
+
+        _screenFader.Fade(isFadingIn:false);
+
+        TruckGameObject.transform.position = _truckLocation.position;
+        TruckGameObject.transform.rotation = _truckLocation.rotation;
+        _playerController.transform.position = _playerLocation.position;
+
+        yield return new WaitForSeconds(.5f);
+
+        _screenFader.Fade();
+
+
+        yield return null;
     }
 }
