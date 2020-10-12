1e10c7c0d7f2604a3b17e04c071a63bc995d8547
Dave <dladr@hotmail.com>
2020-10-12 13:08:36 -0700

    Stranger driving dialogue & mailbox #3

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Helpers;
+using TMPro;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public string Dialogue;
+
+    public Color Color;
+
+    public FontStyles FontStyles;
+
+    public bool HasTriggered;
+
+    private TextModifier _textModifier;
+
+    private void Awake()
+    {
+        _textModifier = SingletonManager.Get<TextModifier>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!HasTriggered && other.CompareTag("Truck"))
+        {
+            HasTriggered = true;
+            _textModifier.UpdateTextTrio(Dialogue, Color, FontStyles);
+            _textModifier.AutoTimeFades();
+        }
+    }
+}
