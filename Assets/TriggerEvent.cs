700ad9586fea6f02e21c7279f0cb5a43f73457c8
Dave <dladr@hotmail.com>
2020-10-12 15:42:20 -0700

    fixing mirrors, etc.

---
+++ Assets/TriggerEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent TheEvent;
+
+    public bool HasTriggered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Truck") && !HasTriggered)
+        {
+            HasTriggered = true;
+            TheEvent.Invoke();
+        }
+    }
+}
